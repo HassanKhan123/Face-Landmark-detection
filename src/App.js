055bf4162d9a0d70e807888fd1a75bf30d32a298
HassanKhan123 <hk2602809@gmail.com>
2020-12-18 00:42:48 +0500

    tensorflow integrated for face landmark detection

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
+import * as facemesh from '@tensorflow-models/facemesh';
+// import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection';
+// import * as tf from '@tensorflow/tfjs';
+import Webcam from 'react-webcam';
+
 import './App.css';
+import { drawMesh } from './utils';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const videoConstraints = {
+    facingMode: 'user',
+  };
+
+  const runFacemesh = async () => {
+    const net = await facemesh.load({
+      inputResolution: {
+        width: 640,
+        height: 480,
+      },
+      scale: 0.8,
+    });
+    console.log(net);
+
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== 'undefined' &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+      try {
+        const face = await net.estimateFaces(video);
+        console.log(face);
+
+        const ctx = canvasRef.current.getContext('2d');
+        drawMesh(face, ctx);
+      } catch (error) {
+        console.log('err===', error);
+      }
+    }
+  };
+
+  runFacemesh();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className='App-header'>
+      <Webcam
+        videoConstraints={videoConstraints}
+        ref={webcamRef}
+        style={{
+          position: 'absolute',
+          margin: '0 auto',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          zIndex: 10,
+          width: 640,
+          height: 480,
+          border: '1px solid',
+        }}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: 'absolute',
+          margin: '0 auto',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          zIndex: 10,
+          width: 640,
+          height: 480,
+        }}
+      />
+    </header>
   );
 }
 
